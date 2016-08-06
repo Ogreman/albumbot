02a872f5b0ae11e2ffb1c7974216fc38eb1bf01f
Jamie Cox <jamie@potatolondon.com>
2016-08-07 00:02:48 +0100

    rev

--- plugins/process.py
+++ plugins/process.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 # don't convert to ascii in py2.7 when creating string to return
-import json
 import requests
 import os
 
@@ -12,12 +11,12 @@ crontable.append([timeout, "process_albums"])
 
 message = "I'm going to make sure any new albums have been processed in the Doomlist."
 url = "https://doomlist.herokuapp.com/slack/process"
-data = {'token': os.environ.get('SLACK_APP_TOKEN'), 'user_id': "U1YUJELR3"}
+data = {'token': os.environ.get('SLACK_APP_TOKEN'), 'user_id': "U1YUJELR3"} 
 channel = "C0A1YLG3X"
 
 
 def process_albums():
     outputs.append([channel, message])
-    response = requests.post(url, data=json.dumps(data))
+    response = requests.post(url, data=data)
     if not response.ok or response.content == '':
         outputs.append([channel, "Odd... Something went wrong."])
