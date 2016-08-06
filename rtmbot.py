d9a827f1e5e43ced9c8543eade00fc0226348e5c
Jamie Cox <jamie@potatolondon.com>
2016-08-06 22:46:12 +0100

    proc

--- rtmbot.py
+++ rtmbot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 from argparse import ArgumentParser
 
@@ -6,21 +7,26 @@ import yaml
 from rtmbot import RtmBot
 
 
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument(
-        '-c',
-        '--config',
-        help='Full path to config file.',
-        metavar='path'
-    )
-    return parser.parse_args()
+# def parse_args():
+#     parser = ArgumentParser()
+#     parser.add_argument(
+#         '-c',
+#         '--config',
+#         help='Full path to config file.',
+#         metavar='path'
+#     )
+#     return parser.parse_args()
 
 # load args with config path
-args = parse_args()
-config = yaml.load(open(args.config or 'rtmbot.conf', 'r'))
+# args = parse_args()
+# config = yaml.load(open(args.config or 'rtmbot.conf', 'r'))
+config = {
+    'SLACK_TOKEN': os.environ.get('SLACK_TOKEN'),
+    'DEBUG': os.environ.get('DEBUG', True),
+}
 bot = RtmBot(config)
-try:
-    bot.start()
-except KeyboardInterrupt:
-    sys.exit(0)
+bot.start()
+# try:
+    # bot.start()
+# except KeyboardInterrupt:
+    # sys.exit(0)
